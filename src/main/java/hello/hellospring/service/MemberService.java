8ad2c44db864ae6ea8206c4eafd508ce15a0c22d
minhee <minhee810@naver.com>
2024-03-22 02:34:05 +0900

    FEAT : MemberServiceTest 작성중

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,8 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원가입
@@ -27,7 +30,7 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName()).ifPresent(member1 -> {
-            throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
+            throw new IllegalStateException("이미 존재하는 사용자입니다.");
         });
     }
 
